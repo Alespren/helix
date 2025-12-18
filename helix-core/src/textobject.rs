275bbcabcfe93a4b2370a7c8898e77ea72447b45
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2025-12-18 21:11:41 +0800

    textobjects: most rotate backwards

--- helix-core/src/textobject.rs
+++ helix-core/src/textobject.rs
@@ -250,6 +250,7 @@ fn textobject_pair_surround_impl(
             TextObject::Movement => unreachable!(),
         })
         .unwrap_or(range)
+        .with_direction(Direction::Backward)
 }
 
 /// Transform the given range to select text objects based on tree-sitter.
@@ -287,7 +288,7 @@ pub fn textobject_treesitter(
         let start_char = slice.byte_to_char(start_byte);
         let end_char = slice.byte_to_char(end_byte);
 
-        Some(Range::new(start_char, end_char))
+        Some(Range::new(start_char, end_char).with_direction(Direction::Backward))
     };
     get_range().unwrap_or(range)
 }
